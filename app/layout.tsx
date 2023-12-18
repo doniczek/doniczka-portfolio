3c6fd383365a6da2b6da01ac9b8a342de9969740
Miłosz Strzałkowski <64070490+doniczek@users.noreply.github.com>
2023-12-18 01:25:27 +0100

    tegii

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-
   return (
     <html lang="pl" className='scroll-smooth'>
        <body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 'use client'
-import Image from 'next/image'
-import React, { useState, useEffect } from 'react'
+import * as React from "react";
 import Head from 'next/head'
+import { NextUIProvider } from "@nextui-org/react";
+import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image, Chip } from "@nextui-org/react";
+import { Avatar, AvatarGroup, AvatarIcon } from "@nextui-org/avatar";
 export default function Home() {
-  const [currentTime, setCurrentTime] = useState<string>(getCurrentTime());
+  const [currentTime, setCurrentTime] = React.useState<string>(getCurrentTime());
 
-  useEffect(() => {
+
+  React.useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentTime(getCurrentTime());
     }, 500);
@@ -27,25 +30,63 @@ export default function Home() {
   }
 
   return (
-    <div className="min-h-screen">
-      <section className="container mx-auto my-8 p-8 bg-white dark:bg-blue-500 rounded-lg shadow-2xl">
-        <h2 className="text-2xl font-bold mb-4"><a href='https://github.com/doniczek' className='hover:text-gray-300'>@doniczka</a> <span className='text-gray-500'>{currentTime}</span></h2>
-        <p className="text-gray-200">
-          Witam na mojej stronie! Nazywam się Miłosz i jestem pasjonatem programowania.
-          Posiadam umiejętności w <span className="text-blue-400">typescript</span>, <span className='text-yellow-400'>javascript</span>
-        </p><br></br>
-        <h2>Technologie z których korzystam:</h2>
-      </section>
-      <section className="container mx-auto my-8 p-8 bg-white rounded-lg shadow-2xl dark:bg-blue-400">
-        <h2 className="text-2xl font-bold mb-4">Projekty</h2>
-        <div className="mb-6">
-          <h3 className="text-xl font-bold mb-2">Nazwa Projektu 1</h3>
-          <p className="text-gray-700">Opis projektu. [Dodaj krótki opis projektu oraz użyte technologie.]</p>
-          <a href="#" className="text-blue-500 hover:underline">Zobacz więcej</a>
-        </div>
-        { }
-      </section>
-    </div>
+    <NextUIProvider>
+      <div className="min-h-screen flex items-center justify-center">
+        <Card className="max-w-[1200px] max-h-[200px] h-94">
+          <CardHeader className="flex gap-3">
+            <Image
+              alt="nextui logo"
+              height={90}
+              radius="lg"
+              src="https://avatars.githubusercontent.com/u/64070490?s=200&v=4"
+              width={90}
+            />
+            <div className="flex flex-col">
+              <p className="text-md">@doniczka | Miłosz Strzałkowski</p>
+              <p className="text-small text-default-500">doniczka.lol</p>
+              <div className="flex gap-2">
+                <Chip
+                  avatar={<Avatar name="TS" src='https://th.bing.com/th/id/R.99a29ede35ec2ddf1f968bcdb17dbfdd?rik=XCSlxNe4MX5SOg&pid=ImgRaw&r=0d' />}
+                  variant="flat"
+                  color="primary"
+                >
+                  Typescript
+                </Chip>
+                <Chip avatar={<Avatar name="JS" src='https://th.bing.com/th/id/R.41ab93134d63c714a37f5c5a6c560faa?rik=VidAiYGI4alD%2bw&pid=ImgRaw&r=0' />} variant="flat" color="warning"
+                >
+              Javascript</Chip>
+              <Chip avatar={<Avatar name="React" src='https://cdn.discordapp.com/attachments/623564778379673630/1186093293512167515/OIP.png?ex=6591fe75&is=657f8975&hm=58399a4e5a2fce2971b8fced2a8b67fa2c18467154f404fbbf7240b85187014e&' />} variant="flat" color="secondary"
+                >
+              React</Chip>
+              
+              </div>
+            </div>
+          </CardHeader>
+          <Divider />
+          <CardBody>
+            <p>defeloper z pabianic proste co nie? tego teges.</p>
+          </CardBody>
+          <Divider />
+          <CardFooter className='flex gap-3'>
+            <Link
+              isExternal
+              showAnchorIcon
+              href="https://github.com/doniczek"
+            >
+              github
+            </Link>
+            <Link
+              isExternal
+              showAnchorIcon
+              href="https://discord.com/users/485414045516562443"
+            >
+              discord
+            </Link>
+        
+          </CardFooter>
+        </Card>
 
+      </div>
+    </NextUIProvider>
   );
 }
